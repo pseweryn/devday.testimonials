d77948265e09233e5e0de5c06e6c7ae99a54a3d3
pseweryn <pawel.seweryn@pl.abb.com>
2013-10-20 01:17:39 +0200

    closing icons

--- js/script.js
+++ js/script.js
@@ -6,11 +6,12 @@ $(function () {
     $('.tileContainer > div').each(function (i) {
         $(this).click(function () {
             var $cube = $('.cube.c' + i + '');
-            if ($cube.is( ":visible" )){
-                    $cube.slideUp('fast');
-                } else {
-                    $cube.slideDown('fast');
-                }
+            if ($cube.is(":visible")) {
+                $cube.slideUp('fast');
+            } else {
+                $cube.slideDown('fast');
+                $('.closeBack').addClass('closeC' + i + '');
+            }
         })
     })
 }); 
