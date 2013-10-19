072c55bc050d8ffe27be82e374b294a8b12687c0
pseweryn <pawel.seweryn@pl.abb.com>
2013-10-19 21:46:15 +0200

    cube divs with basic slideup

--- js/script.js
+++ js/script.js
@@ -2,7 +2,18 @@
 
 */
 
-
+$(function () {
+    $('.tileContainer > div').each(function (i) {
+        $(this).click(function () {
+            var $cube = $('.cube.c' + i + '');
+            if ($cube.is( ":visible" )){
+                    $cube.slideUp('fast');
+                } else {
+                    $cube.slideDown('fast');
+                }
+        })
+    })
+}); 
 
 
 
