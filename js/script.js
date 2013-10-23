eca6d37c60ef2d1a350bf7edae763b07272971e2
pseweryn <pawel.seweryn@pl.abb.com>
2013-10-23 19:40:07 +0200

    arrows click action first version + see you icon/div

--- js/script.js
+++ js/script.js
@@ -3,6 +3,30 @@
 */
 
 $(function () {
+
+    function hideCloseIcon() {
+        $('.logoContainer > div').not('.close').css('visibility', '');
+        $('#closeBack').removeClass().addClass('icon');
+        $('.closeFront').hide();
+    }
+
+    //TODO: ugly! refactor soon!
+    function getCindex($cube) {
+        if ($cube.hasClass('c0')) {
+            return 0;
+        }
+        else if ($cube.hasClass('c1')) {
+            return 1;
+        }
+        else if ($cube.hasClass('c2')) {
+            return 2;
+        }
+        else if ($cube.hasClass('c3')) {
+            return 3;
+        }
+        return 0;
+    }
+
     $('.pic').magnificPopup({
         delegate: 'a',
         type: 'image',
@@ -25,9 +49,7 @@ $(function () {
     $('.close').click(function () {
         $('.cube').hide();
         $('.alpha60').show();
-        $('.logoContainer > div').not('.close').css('visibility', '');
-        $('#closeBack').removeClass().addClass('icon');
-        $('.closeFront').hide();
+        hideCloseIcon();
     })
 
     $('.filters h4').click(function () {
@@ -53,6 +75,34 @@ $(function () {
         var $radio = $(this).next().find('input[type="radio"]');
         $radio.prop('checked', true);
         $('input[type="radio"]').not($radio).prop('checked', false);
+    })
+    $('.arrowContainer').click(function () {
+        var $a = $(this);
+        var $parent = $a.parent();
+        var $cubes = $('.cube');
+        $parent.hide();
+        if ($a.hasClass('left')) {
+            var $prev = $parent.prev();
+            //var pcIndex = $cubes.indexOf($prev);
+            if ($prev.hasClass('alpha60')) {
+                hideCloseIcon();
+            } else {
+                $('#closeBack').removeClass().addClass('icon').addClass('closeC' + getCindex($prev) + '');
+            }
+            $prev.show();
+        }
+        else {
+            var $next = $parent.next();
+            //var ncIndex = $cubes.indexOf($next);
+            if ($next.hasClass('cube')) {
+                $('#closeBack').removeClass().addClass('icon').addClass('closeC' + getCindex($next) + '');
+                $next.show();
+            } else {
+                $('.alpha60').show();
+                hideCloseIcon();
+            }
+        }
+
     })
 }); 
 
