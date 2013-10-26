e8952b8ead7e485d3e92461d5ce79e37951f943e
pseweryn <pawel.seweryn@pl.abb.com>
2013-10-26 22:34:49 +0200

    animations first version

--- js/script.js
+++ js/script.js
@@ -4,27 +4,40 @@
 
 $(function () {
 
+    var offsetnote = 78;
+    var $main = $('#main');
+    var $pages = $main.children('div.page');
+    var $close = $('.close');
+    var current = 0;
+    var $currPage = "";
+    var $nextPage = "";
+    var isAnimating = false;
+    var endCurrPage = false;
+    var endNextPage = false;
+    var animEndEventNames = {
+        'WebkitAnimation': 'webkitAnimationEnd',
+        'OAnimation': 'oAnimationEnd',
+        'msAnimation': 'MSAnimationEnd',
+        'animation': 'animationend'
+    };
+    // animation end event name
+    var animEndEventName = animEndEventNames[Modernizr.prefixed('animation')];
+    // support css animations
+    var support = Modernizr.cssanimations;
+
+    function setPageClasses() {
+        $pages.each(function () {
+            var $page = $(this);
+            $page.data('originalClassList', $page.attr('class'));
+        });
+
+        $pages.eq(current).removeClass().addClass('large-12 columns overlay alpha60 page').addClass('page-current');
+    }
+
     function hideCloseIcon() {
         $('.logoContainer > div').not('.close').css('visibility', '');
         $('#closeBack').removeClass().addClass('icon');
-        $('.closeFront').hide();
-    }
-
-    //TODO: ugly! refactor soon!
-    function getCindex($cube) {
-        if ($cube.hasClass('c0')) {
-            return 0;
-        }
-        else if ($cube.hasClass('c1')) {
-            return 1;
-        }
-        else if ($cube.hasClass('c2')) {
-            return 2;
-        }
-        else if ($cube.hasClass('c3')) {
-            return 3;
-        }
-        return 0;
+        $('.closeFront').fadeOut();
     }
 
     function initLightbox() {
@@ -39,24 +52,25 @@ $(function () {
     }
 
     function tilesClick() {
-        $('.tileContainer > div').each(function (i) {
-            $(this).click(function () {
-                var $cube = $('.cube.c' + i + '');
-                $cube.slideDown('fast', function () {
-                    $('.alpha60').hide();
-                    $('.logoContainer > div').not('.close').css('visibility', 'hidden');
-                    $('#closeBack').addClass('closeC' + i + '');
-                    $('.closeFront').show();
-                });
-            })
+
+        $('.tileContainer > div').click(function () {
+            var tile_ind = $(this).attr('data-index');
+            $('.logoContainer > div').not('.close').css('visibility', 'hidden');
+            $('#closeBack').addClass('closeC' + tile_ind + '');
+
+            if (isAnimating) {
+                return false;
+            }
+            openPage('top', tile_ind);
         })
     }
 
     function closeClick() {
-        $('.close').click(function () {
-            $('.cube').hide();
-            $('.alpha60').show();
-            hideCloseIcon();
+        $close.click(function () {
+            if (isAnimating) {
+                return false;
+            }
+            openPage('close', 0);
         })
     }
 
@@ -96,39 +110,135 @@ $(function () {
         $('.arrowContainer').click(function () {
             var $a = $(this);
             var $parent = $a.parent();
-            var $cubes = $('.cube');
-            $parent.hide();
             if ($a.hasClass('left')) {
                 var $prev = $parent.prev();
-                //var pcIndex = $cubes.indexOf($prev);
+                var prev_ind = $prev.attr('data-index');
                 if ($prev.hasClass('alpha60')) {
+                    openPage('close', 0);
                     hideCloseIcon();
                 } else {
-                    $('#closeBack').removeClass().addClass('icon').addClass('closeC' + getCindex($prev) + '');
+                    $('#closeBack').removeClass().addClass('icon').addClass('closeC' + prev_ind + '');
+                    openPage('top', prev_ind);
                 }
-                $prev.show();
+
             }
             else {
                 var $next = $parent.next();
-                //var ncIndex = $cubes.indexOf($next);
-                if ($next.hasClass('cube')) {
-                    $('#closeBack').removeClass().addClass('icon').addClass('closeC' + getCindex($next) + '');
-                    $next.show();
-                } else {
-                    $('.alpha60').show();
+                var next_ind = $next.attr('data-index');
+                if (!next_ind) {
+                    openPage('close', 0);
                     hideCloseIcon();
+                } else {
+                    $('#closeBack').removeClass().addClass('icon').addClass('closeC' + next_ind + '');
+                    openPage('top', next_ind);
                 }
             }
         })
     }
 
+    function openPage(animation, index) {
+
+        $currPage = $pages.eq(current);
+        current = index;
+
+        $nextPage = $pages.eq(current).addClass('page-current'),
+		outClass = '', inClass = '';
+
+        if (index == 0) {
+
+            hideCloseIcon();
+            //$("#page_main").scrollTop(offsetnote);
+
+        }
+        else {
+
+            //$nextPage.scrollTop(offsetnote);
+            $('.closeFront').delay(800).fadeIn('slow');
+
+        }
+
+        if (isAnimating) {
+            return false;
+        }
+
+        isAnimating = true;
+
+        switch (animation) {
+
+            case 'close':
+                outClass = 'pt-page-rotateFoldBottom';
+                inClass = 'pt-page-moveFromTopFade';
+                break;
+            case 'top':
+                outClass = 'pt-page-moveToTopFade';
+                inClass = 'pt-page-rotateUnfoldBottom';
+                break;
+
+        }
+
+        $currPage.addClass(outClass).bind(animEndEventName, onInAnimation);
+        $nextPage.addClass(inClass).bind(animEndEventName, onOutAnimation);
+
+        if (!support) {
+            onEndAnimation($currPage, $nextPage);
+        }
+
+    }
+
+    function onInAnimation() {
+
+        endCurrPage = true;
+        if (endNextPage) {
+            onEndAnimation($currPage, $nextPage);
+        }
+
+        $currPage.unbind(animEndEventName, onInAnimation);
+    }
+
+    function onOutAnimation() {
+
+        endNextPage = true;
+        if (endCurrPage) {
+            onEndAnimation($currPage, $nextPage);
+        }
+
+        $nextPage.unbind(animEndEventName, onOutAnimation);
+    }
+
+    function onEndAnimation($outpage, $inpage) {
+        endCurrPage = false;
+        endNextPage = false;
+        resetPage($outpage, $inpage);
+        isAnimating = false;
+    }
+
+    function noAnimation() {
+        isAnimating = true;
+        endCurrPage = true;
+        endNextPage = true;
+        endCurrPage = false;
+        endNextPage = false;
+        resetPage($currPage, $nextPage);
+        isAnimating = false;
+    }
+
+    function resetPage($outpage, $inpage) {
+        $outpage.attr('class', $outpage.data('originalClassList'));
+        $inpage.attr('class', $inpage.data('originalClassList') + ' page-current');
+    }
+
     function init() {
+
+        setPageClasses();
+
         closeClick();
         filtersClick();
-        formRadioChange();
         formIconClick();
         arrowClick();
         tilesClick();
+
+        formRadioChange();
+
         initLightbox();
     }
 
